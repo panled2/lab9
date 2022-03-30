cadd270a382412ff51cd97973be5e91932c15a4f
panled2 <jacky.pan32@gmail.com>
2022-03-30 00:34:54 -0400

    v1.7

--- server_html/controllers/homecontroller.js
+++ server_html/controllers/homecontroller.js
@@ -1,4 +1,25 @@
-exports.respondwithimage = (req,res) => {
-    let paramsImage =req.params.myImage
-    res.render("images", { image: paramsImage})
-}
+exports.initialReq = (req,res) => {
+    res.render("index");
+};
+
+exports.paramImage = (req,res) => {
+    let imagenum = req.params.imageNumber;
+    if (imagenum == 1) {
+        res.render("image1");
+    };
+    if (imagenum == 2) {
+        res.render("image2");
+    }
+    if (imagenum == 3) {
+        res.render("image3")
+    }
+    if (imagenum == 4) {
+        res.render("image3")
+    }
+    if (imagenum == 5) {
+        res.render("image3")
+    }
+    if (imagenum == 6) {
+        res.render("image3")
+    }
+};
--- server_html/index.js
+++ server_html/index.js
@@ -16,10 +16,8 @@ app.get('/home', (req,res)=>{
     res.render('home')
 });
 
-app.get("/images/:images", (req,res) =>{
-    let image= req.params.images;
-    res.sendFile(`/server_html/public/images/${image}.png`)
-});
+app.get("/home", homeController.initialReq);
+app.get("/images/:imagenum", homeController.paramImage);
 
 app.listen(app.get("port"), ()=>{
     console.log(`Server running at http://localhost:${app.get("port")}`);
